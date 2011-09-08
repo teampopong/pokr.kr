761d8abcfde1a168a6f084468d483390c0f929f4
Cheol Kang <kang@cheol.net>
2011-09-09 00:35:20 +0900

    Intimacy: 검색창의 자동검색에 의원 정보 포함시킴

--- web/intimacy/module.js
+++ web/intimacy/module.js
@@ -1,8 +1,26 @@
 module.load = function (path) {
 	module.display('frame.html').then(function () {
+		// FIXME: remove duplicated data loading
+		module.loadJSON('members', 'members.json', false).then(function () {
+			var parties = _.groupBy(module.members, function (member) {
+				return member.party;
+			});
+
+			module.template('list.html').then(
+					function (template) {
+						var context = { parties: parties };
+						$('#member-list').html(template(context));
+					},
+					function (error) {
+						alert('error loading list');
+					}
+					);
+
+			module.loadJS('chosen.jquery.js', function () {
+				$('.chzn-select').chosen();
+			});
+		});
+
 		module.loadCSS('chosen.css');
-		module.loadJS('chosen.jquery.js', function () {
-					$('.chzn-select').chosen();
-				});
 	});
 };
