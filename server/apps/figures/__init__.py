5f5b326d63d3ec7d3541778df35201040b4f18c2
Cheol Kang <cornchz@gmail.com>
2012-05-29 00:41:45 +0900

    정치인 정보 기본 API 작성

--- server/apps/figures/__init__.py
+++ server/apps/figures/__init__.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-from flask import Blueprint, current_app
+from flask import Blueprint, g
 
 app = Blueprint('figures', __name__)
 
 @app.route('/')
 def figures():
-    # can use current_app.db, current_app.cache
-    return ''
+    return str(list(g.db['figures'].find()))
+
+@app.route('/<int:id_>')
+def figure(id_):
+    return str(g.db['figures'].find_one({
+        'id': id_
+        }))
