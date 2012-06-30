bdb40ec32f82ea60a52f79ef25863cb739b72a4a
Cheol Kang <cornchz@gmail.com>
2012-06-30 13:44:34 +0900

    인명사전 검색 기능 구현

--- server/apps/people/__init__.py
+++ server/apps/people/__init__.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 
-from flask import Blueprint, g, render_template
+from flask import Blueprint, g, redirect, render_template, request, url_for
 
-app = Blueprint('people', __name__, template_folder='templates', static_folder='static')
+app = Blueprint('people', __name__,
+        template_folder='templates', static_folder='static')
 
-@app.route('/')
-def people():
-    return 'people'
-    # return g.db['people'].find()
+# 루트
+@app.route('/', methods=['GET'])
+def main():
+    query = request.args.get('q', None)
+    if query is not None:
+        return redirect(url_for('people.search', query=query))
 
-@app.route('/<int:id_>')
+    return render_template('person.html', menu='people')
+
+# 이름으로 검색
+@app.route('/q/<query>', methods=['GET'])
+def search(query):
+    # TODO: validation & sanitization
+    # TODO: 처음엔 몇 개만 받아오고, '더 보기'를 누르면 나머지를 가져옴
+    results = list(g.db['people'].find({
+        'name': {'$regex': query}
+        }))
+    num_results = len(results)
+    return render_template('search-results.html', menu='people', results=results,
+            query=query, num_results=num_results)
+
+# 사람
+@app.route('/<int:id_>', methods=['GET'])
 def person(id_):
     person = g.db['people'].find_one({
         'id': id_
         })
 
     if person:
-        return render_template('person-found.html', person=person, menu='people')
+        return render_template('person-found.html', menu='people', person=person)
     else:
         return render_template('person-not-found.html', menu='people'), 404
