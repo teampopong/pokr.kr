0f5397cc75147fd5e526d6ba84411af744930751
Cheol Kang <cornchz@gmail.com>
2013-03-30 17:09:51 +0900

    정당 페이지에 현 의원만 표시

--- models/party.py
+++ models/party.py
@@ -1,6 +1,7 @@
 from sqlalchemy import CHAR, Column, Integer, Unicode
 from sqlalchemy.orm import backref, relationship
 from database import Base
+from models.person import Person
 from models.party_affiliation import party_affiliation
 
 class Party(Base):
@@ -18,3 +19,12 @@ class Party(Base):
         self.name = name
         if color:
             self.color = color
+
+    @property
+    def current_members(self):
+        return Person.query.join(party_affiliation,
+                                 Person.id == party_affiliation.c.person_id)\
+                           .filter(party_affiliation.c.is_current_member == True)\
+                           .join(Party,
+                                 Party.id == party_affiliation.c.party_id)\
+                           .filter(Party.id == self.id)
