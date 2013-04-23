1c9a06430457c5358405749b9b2c0c43fa44e07c
Cheol Kang <steel@popong.com>
2013-04-23 18:38:33 +0900

    Auto-migrate DB on starting server

--- database.py
+++ database.py
@@ -1,16 +1,23 @@
+import os.path
+
+from alembic.config import Config as AlembicConfig
+from alembic import command
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 import settings
 
+
+alembic_cfg_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                'alembic.ini')
 engine = create_engine(settings.sqlalchemy_uri)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
-
 Base = declarative_base()
 Base.query = db_session.query_property()
+
 
 def init_db(app):
     # import all modules here that might define models so that
@@ -18,6 +25,9 @@ def init_db(app):
     # you will have to import them first before calling init_db()
     import models
     Base.metadata.create_all(bind=engine)
+
+    alembic_cfg = AlembicConfig(alembic_cfg_path)
+    command.upgrade(alembic_cfg, 'head')
 
     @app.teardown_request
     def shutdown_session(exception=None):
