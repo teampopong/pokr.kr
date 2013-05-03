33b7e07fce42a0b56b7374464679ecd886bb19b1
Lucy Park <me@lucypark.kr>
2013-05-03 19:20:29 +0900

    모아보기에서 tab 제목 보여주기

--- static/js/person.js
+++ static/js/person.js
@@ -4,10 +4,15 @@ $(function () {
     // FIXME: .tab-aggregated should be #id
     // but bootstrap seems not to support that
     $('.tab-aggregated').click(function () {
+        $('.tab-content h3').show();
         $('#member-view .tab-pane').addClass('active');
         $('#member-view .nav-tabs li').removeClass('active');
         $(this).parent('li').addClass('active');
         return false;
+    });
+
+    $('.nav-tabs a:not(.tab-aggregated)').click(function () {
+        $('.tab-content h3').hide();
     });
 });
 
