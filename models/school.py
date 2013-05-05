aa7985a6422e83b30ed5c2e74c2ffdb81cda273e
Lucy Park <me@lucypark.kr>
2013-05-05 23:33:55 +0900

    Show search results on school page

--- models/school.py
+++ models/school.py
@@ -1,14 +1,15 @@
 from sqlalchemy import Column, Integer, Unicode
 from sqlalchemy.orm import backref, relationship
 from database import Base
-from models.education import education
+from models.person import Person
 
 class School(Base):
     __tablename__ = 'school'
 
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(Unicode(40))
-    alumnis = relationship('Person',
-            secondary=education,
-            backref='school')
+
+    @property
+    def alumni(self):
+        return Person.query.filter(Person.education_id.any(str(self.id))).all()
 
--- views/school.py
+++ views/school.py
@@ -2,17 +2,12 @@
 # -*- encoding: utf-8 -*-
 
 from flask import g, redirect, render_template, request, url_for
-from sqlalchemy import or_
 from sqlalchemy.orm.exc import NoResultFound
 from werkzeug.local import LocalProxy
-
-from database import db_session
-from models.person import Person
+from models.school import School
 
 
-def register(app):
-
-    # 루트
+def register(app): # 루트
     @app.route('/school/', methods=['GET'])
     def person_main():
         query = request.args.get('q', None)
@@ -26,6 +21,8 @@ def register(app):
     # FIXME: 별도의 함수로 빼지 말고 통합 검색
     @app.route('/school/<education_id>', methods=['GET'])
     def school(education_id):
-        results = Person.query.filter(Person.education_id.any(education_id)).all()
-        return render_template('search-results.html', results=results,
-                query=education_id)
+        try:
+            school = School.query.filter_by(id=education_id).one()
+        except NoResultFound, e:
+            return render_template('not-found.html'), 404
+        return render_template('school.html', school=school)
