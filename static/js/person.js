ace3ace7bd97d5b18c49e207d1d67a5c7063dc4c
Cheol Kang <cornchz@gmail.com>
2013-05-05 18:41:45 +0900

    Adopt vertically tabbed layout w/ scrollspy

--- static/js/person.js
+++ static/js/person.js
@@ -1,23 +1,28 @@
 (function () {
 
-$(function () {
-    // FIXME: .tab-aggregated should be #id
-    // but bootstrap seems not to support that
-    $('.tab-aggregated').click(function () {
-        $('.tab-content h3').show();
-        $('#member-view .tab-pane').addClass('active');
-        $('#member-view .nav-tabs li').removeClass('active');
-        $(this).parent('li').addClass('active');
-        return false;
-    });
-
-    $('.nav-tabs a:not(.tab-aggregated)').click(function () {
-        $('.tab-content h3').hide();
-    });
+$('#person-section-tabs a').click(function () {
+    var href = $(this).attr('href'),
+        $elem = $(href);
+    selectTab(href);
+    $('#spec-container').animate({
+        scrollTop: $elem.position().top
+    }, 300);
+    return false;
 });
 
 $(window).load(function () {
+    $('#spec-container').scrollspy();
     $('.tooltipped').tooltip();
 });
+
+$('#spec-container').scroll(function () {
+    $('#spec-container').scrollspy('refresh');
+});
+
+function selectTab(target) {
+    var $list = $('#person-section-tabs li');
+    $list.removeClass('active');
+    $list.children('[href="'+target+'"]').parent('li').addClass('active');
+}
 
 }());
