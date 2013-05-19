b51618f1527af0723d31147237e90739a8be9795
Cheol Kang <cornchz@gmail.com>
2013-05-19 12:36:31 +0900

    Disable tooltips on moblie

--- static/js/person.js
+++ static/js/person.js
@@ -1,40 +1,42 @@
 (function () {
 
-$('#person-section-tabs a').click(function () {
-    var href = $(this).attr('href');
-    gotoTab(href);
-    return false;
-});
-
-$('#spec-container').scrollspy({
-        'data-spy': 'scroll',
-        'data-target': '#person-section-tabs',
-        'offset': 100
-    })
-    .on('activate.changehash', function () {
-        var target = $('#person-section-tabs li.active a').attr('href');
-        selectTab(target);
+if (!isMobile) {
+    $('#person-section-tabs a').click(function () {
+        var href = $(this).attr('href');
+        gotoTab(href);
+        return false;
     });
 
-function gotoTab(target) {
-    selectTab(target);
+    $('#spec-container').scrollspy({
+            'data-spy': 'scroll',
+            'data-target': '#person-section-tabs',
+            'offset': 100
+        })
+        .on('activate.changehash', function () {
+            var target = $('#person-section-tabs li.active a').attr('href');
+            selectTab(target);
+        });
 
-    var $elem = $(target);
-    $('#spec-container').animate({
-        scrollTop: $elem.position().top
-    }, 300);
-}
+    function gotoTab(target) {
+        selectTab(target);
 
-function selectTab(target) {
-    var $list = $('#person-section-tabs li');
-    $list.removeClass('active');
-    $list.children('[href="'+target+'"]').parent('li').addClass('active');
+        var $elem = $(target);
+        $('#spec-container').animate({
+            scrollTop: $elem.position().top
+        }, 300);
+    }
 
-    if (location.hash != target) {
-        if (history.pushState) {
-            history.pushState({}, null, target);
-        } else {
-            location.hash = target;
+    function selectTab(target) {
+        var $list = $('#person-section-tabs li');
+        $list.removeClass('active');
+        $list.children('[href="'+target+'"]').parent('li').addClass('active');
+
+        if (location.hash != target) {
+            if (history.pushState) {
+                history.pushState({}, null, target);
+            } else {
+                location.hash = target;
+            }
         }
     }
 }
--- static/js/polidic.js
+++ static/js/polidic.js
@@ -1,28 +1,32 @@
 (function () {
 
 // event triggers
-$('a.person-link').live('click.linkPerson', function (e) {
-    var $this = $(this);
+if (!isMobile) {
+    $('a.person-link').live('click.linkPerson', function (e) {
+        var $this = $(this);
 
-    // Preserve hashtag if the current page is of a person
-    if (window.currentPage == 'person' && location.hash) {
-        location.href = $this.attr('href') + location.hash;
-        return false;
-    }
-});
+        // Preserve hashtag if the current page is of a person
+        if (window.currentPage == 'person' && location.hash) {
+            location.href = $this.attr('href') + location.hash;
+            return false;
+        }
+    });
+}
 
 /* Warm-up scripts */
 $(window).load(onLoad);
 
 function onLoad() {
     $('.person-img').clipImage();
-    $('.tooltipped:not(.tooltipped-delay)').tooltip();
-    $('.tooltipped-delay').tooltip({
-        delay: {
-            show: 3000,
-            hide: 0
-        }
-    });
+    if (!isMobile) {
+        $('.tooltipped:not(.tooltipped-delay)').tooltip();
+        $('.tooltipped-delay').tooltip({
+            delay: {
+                show: 3000,
+                hide: 0
+            }
+        });
+    }
 };
 
 }());
