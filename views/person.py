827cc2db4fb470527a0017a40bf571a8c02978fe
Lucy Park <me@lucypark.kr>
2013-05-25 18:19:53 +0900

    Update people submain page

--- views/person.py
+++ views/person.py
@@ -12,6 +12,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.sql.expression import desc
 
 from database import db_session
+from models.candidacy import Candidacy
 from models.person import Person
 from utils.jinja import breadcrumb
 
@@ -27,8 +28,16 @@ def register(app):
     @app.route('/person/', methods=['GET'])
     @breadcrumb(app)
     def person_main():
+        # FIXME: 19
+        assembly_id = int(request.args.get('assembly_id', 19))
         people = Person.query.order_by(desc(Person.id))
-        return render_template('people.html', people=people)
+
+        if assembly_id:
+            people = people.join(Candidacy)\
+                           .filter(Candidacy.age == assembly_id)
+
+        return render_template('people.html', people=people,
+                               assembly_id=assembly_id)
 
     @app.route('/person/all-names.json', methods=['GET'])
     def person_all_names():
