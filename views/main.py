91d2599fb56b0acd54ebda21387c5de1dc3a6141
Cheol Kang <cornchz@gmail.com>
2013-05-29 02:04:25 +0900

    Remove party link list on the leftnav

--- views/main.py
+++ views/main.py
@@ -5,17 +5,11 @@ from flask import redirect, render_template, url_for
 import re
 
 from database import db_session
-from models.party import Party
 from utils.jinja import breadcrumb
 
 year_re = re.compile(r'[1-9][0-9]{3}')
-party_list = db_session.query(Party.name, Party.size).order_by(Party.order).filter(Party.name != u'무소속')
 
 def register(app):
-
-    @app.context_processor
-    def inject_parties():
-        return dict(party_list=party_list)
 
     @app.route('/')
     @breadcrumb(app)
