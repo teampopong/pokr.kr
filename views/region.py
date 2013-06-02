2cc9e6be4f8969ead1d34a009997e61c6085c7a7
Cheol Kang <cornchz@gmail.com>
2013-06-02 21:33:10 +0900

    Navigate regions through korean map

--- views/region.py
+++ views/region.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from flask import redirect, render_template, request, url_for
+from flask import redirect, render_template, render_template_string, request, url_for
 from flask.ext.babel import gettext
 from sqlalchemy import func
 from sqlalchemy.orm.exc import NoResultFound
@@ -15,13 +15,19 @@ def register(app):
     app.views['region'] = 'region_main'
     gettext('region') # for babel extraction
 
+    with app.open_resource('static/images/korea_130_v3.1.svg') as f:
+        korean_map = f.read().decode('utf-8')
+        korean_map = render_template_string(korean_map, width=250)
+
+    top_regions = Region.query\
+                        .filter(func.length(Region.id) < 6)\
+                        .order_by(Region.id).all()
+
     @app.route('/region/', methods=['GET'])
     @breadcrumb(app)
     def region_main():
-        regions = Region.query\
-                        .filter(func.length(Region.id) < 6)\
-                        .order_by(Region.id)
-        return render_template('regions.html', regions=regions)
+        return render_template('regions.html', regions=top_regions,
+                korean_map=korean_map)
 
     @app.route('/region/<region_id>', methods=['GET'])
     @breadcrumb(app, 'region')
