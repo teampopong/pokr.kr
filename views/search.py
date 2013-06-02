7de893a367e7765ca5cc5173885c572e7cb6d95e
Cheol Kang <cornchz@gmail.com>
2013-06-02 13:38:47 +0900

    Show search options as text instead of id integers

--- views/search.py
+++ views/search.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from itertools import chain
 
 from flask import request, render_template
 from sqlalchemy import or_
@@ -23,32 +24,39 @@ def register(app):
     @app.route('/search', methods=['GET'])
     @breadcrumb(app)
     def search():
-        results = {}
-        results['people'] = search_people()
-        results['parties'] = search_parties()
-        results['schools'] = search_schools()
-        results['bills'] = search_bills()
-        results['regions'] = search_regions()
-        return render_template('search-results.html', **results)
+        results, options = {}, {}
+        results['people'] , options['people']  = search_people()
+        results['parties'], options['parties'] = search_parties()
+        results['schools'], options['schools'] = search_schools()
+        results['bills']  , options['bills']   = search_bills()
+        results['regions'], options['regions'] = search_regions()
+        options = dict(chain(*(d.iteritems() for d in options.itervalues())))
+        return render_template('search-results.html', option_texts=options, **results)
 
 
     @if_target('people')
     def search_people():
+        options = {}
         school_id = request.args.get('school_id')
         party_id = request.args.get('party_id')
         candidacy_region_id = request.args.get('candidacy_region_id')
         residence_region_id = request.args.get('residence_region_id')
         if school_id:
+            school = School.query.filter_by(id=school_id).one()
+            options['school_id'] = school.name
             people = Person.query.join(School.alumni)\
                                  .filter(School.id == school_id)
         elif candidacy_region_id:
             region = Region.query.filter_by(id=candidacy_region_id).one()
+            options['candidacy_region_id'] = region.fullname
             people = region.candidates
         elif residence_region_id:
             region = Region.query.filter_by(id=residence_region_id).one()
+            options['residence_region_id'] = region.fullname
             people = region.residents
         elif party_id:
             party = Party.query.filter_by(id=party_id).one()
+            options['party_id'] = party.name
             people = party.current_members
         else:
             people = Person.query
@@ -63,25 +71,28 @@ def register(app):
         if year:
             people = people.filter_by(birthday_year=year)
 
-        return people
+        return (people, options)
 
 
     @if_target('parties')
     def search_parties():
+        options = {}
         parties = Party.query
         if query:
             parties = parties.filter(Party.name.like(u'%{0}%'.format(query)))
-        return parties
+        return (parties, options)
 
     @if_target('schools')
     def search_schools():
+        options = {}
         schools = School.query
         if query:
             schools = schools.filter(School.name.like(u'%{0}%'.format(query)))
-        return schools
+        return (schools, options)
 
     @if_target('bills')
     def search_bills():
+        options = {}
         person_id = request.args.get('person_id')
         assembly_id = request.args.get('assembly_id')
 
@@ -94,16 +105,18 @@ def register(app):
             bills = bills.join(cosponsorship)\
                          .join(Person)\
                          .filter(Person.id == person_id)
+            options['person_id'] = Person.query.filter_by(id=person_id).one().name
 
         if assembly_id:
             bills = bills.filter(Bill.age == assembly_id)
 
-        return bills
+        return (bills, options)
 
     @if_target('regions')
     def search_regions():
+        options = {}
         regions = Region.query.filter(Region.name.like(u'%{0}%'.format(query)))
-        return regions
+        return (regions, options)
 
 
 def if_target(target_):
@@ -111,7 +124,7 @@ def if_target(target_):
         @wraps(f)
         def decorated(*args, **kwargs):
             if target and target != target_:
-                return Party.query.filter(false())
+                return (Party.query.filter(false()), {})
             return f(*args, **kwargs)
         return decorated
     return deco
