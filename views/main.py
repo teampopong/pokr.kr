db31846ce216cbd271246176ed0d347eff6e5a39
Lucy Park <me@lucypark.kr>
2013-06-06 17:57:10 +0900

    Add dashboard to main.html

--- views/main.py
+++ views/main.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from flask import redirect, render_template, url_for
 import re
+from flask import redirect, render_template, url_for
+from sqlalchemy.sql.expression import desc
 
 from database import db_session
+from models.bill import Bill
 from utils.jinja import breadcrumb
 
 year_re = re.compile(r'[1-9][0-9]{3}')
 
 def register(app):
 
+    with app.open_resource('static/images/korea_130_v3.1.svg') as f:
+            korean_map = f.read().decode('utf-8')
+
     @app.route('/')
     @breadcrumb(app)
     def main():
-        return render_template('main.html')
+        bills = Bill.query.filter(Bill.age==19).order_by(desc(Bill.proposed_date))
+        return render_template('main.html', bills=bills, korean_map=korean_map)
 
     @app.route('/favicon.ico')
     def favicon():
