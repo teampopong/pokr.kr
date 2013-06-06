21213ad09197bfe76809ffa0d10ef449f531e279
Lucy Park <me@lucypark.kr>
2013-06-06 14:04:01 +0900

    Add resized class buttons

---
+++ static/images/resize.sh
@@ -0,0 +1,11 @@
+convert -resize 200 default_person_ko_empty.png default_person_ko_empty_200.png
+convert -resize 200 default_person_ko_grey.png default_person_ko_grey_200.png
+
+convert -resize 200 default_bill_ko_empty.png default_bill_ko_empty_200.png
+convert -resize 200 default_bill_ko_grey.png default_bill_ko_grey_200.png
+
+convert -resize 200 default_party_ko_empty.png default_party_ko_empty_200.png
+convert -resize 200 default_party_ko_grey.png default_party_ko_grey_200.png
+
+convert -resize 200 default_region_ko_empty.png default_region_ko_empty_200.png
+convert -resize 200 default_region_ko_grey.png default_region_ko_grey_200.png
