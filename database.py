c38db38f6fad0b35c8ac1081339e229e49119f23
Cheol Kang <cornchz@gmail.com>
2013-06-16 17:26:04 +0900

    Fix alembic-sqlalchemy conflicting bug

--- database.py
+++ database.py
@@ -21,14 +21,14 @@ Base.query = db_session.query_property()
 
 
 def init_db(app):
+    if not is_alembic_head():
+        raise Exception('alembic is not on the head')
+
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
     import models
     Base.metadata.create_all(bind=engine)
-
-    if not is_alembic_head():
-        raise Exception('alembic is not on the head')
 
     @app.teardown_request
     def shutdown_session(exception=None):
