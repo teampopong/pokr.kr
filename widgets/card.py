2565df456ecb290f620ce4dadca19c76b0eeb1af
Cheol Kang <steel@popong.com>
2013-08-12 18:22:36 +0900

    Fix a bug in caching

--- widgets/card.py
+++ widgets/card.py
@@ -7,12 +7,12 @@ from models.person import Person
 
 
 @cache.memoize(24 * 60 * 60)
-def card(person_or_id, **kwargs):
+def card(person_or_id, detailed=False, small=False):
 
     if isinstance(person_or_id, Person):
         person = person_or_id
     else:
         person = Person.query.filter_by(id=person_or_id).first()
 
-    return render_template('widgets/card.html', person=person, **kwargs)
+    return render_template('widgets/card.html', person=person, detailed=detailed, small=small)
 
