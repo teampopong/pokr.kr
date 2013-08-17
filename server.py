cff4c83abfda4ec22c7ace9cbd341c0514ba19ca
Cheol Kang <cornchz@gmail.com>
2013-08-17 19:02:31 +0900

    Revert "Enable caching"
    This reverts commit 0eb42c67b6a1f3e6d5fc18ed6abbf16b595fe1b2.
    
    Conflicts:
    
    	cache.py

--- server.py
+++ server.py
@@ -14,7 +14,6 @@ manager = Manager(app, with_default_commands=False)
 
 def init_modules():
     from flask.ext.assets import Environment as Asset
-    from cache import init_cache
     from database import init_db
     from utils.assets import init_app as init_asset
     from utils.jinja import init_app as init_jinja
@@ -27,7 +26,6 @@ def init_modules():
 
     Asset(app)
     init_asset(app)
-    init_cache(app)
     init_db(app)
     init_jinja(app)
     PopongBabel(app, **BABEL_SETTINGS)
