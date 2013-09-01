4b340e0712956ea44eace7382dd743890958a0fd
Cheol Kang <steel@popong.com>
2013-09-01 13:36:24 +0900

    Revert "Fix a bug in caching"
    This reverts commit 2565df456ecb290f620ce4dadca19c76b0eeb1af.
    
    Conflicts:
    	widgets/card.py

--- widgets/card.py
+++ widgets/card.py
@@ -5,12 +5,12 @@ from flask import render_template
 from models.person import Person
 
 
-def card(person_or_id, detailed=False, small=False):
+def card(person_or_id, **kwargs):
 
     if isinstance(person_or_id, Person):
         person = person_or_id
     else:
         person = Person.query.filter_by(id=person_or_id).first()
 
-    return render_template('widgets/card.html', person=person, detailed=detailed, small=small)
+    return render_template('widgets/card.html', person=person, **kwargs)
 
