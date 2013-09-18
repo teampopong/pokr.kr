b28c050a61cfd8cf1084addc16e663c734c08f83
Cheol Kang <cornchz@gmail.com>
2013-09-18 11:16:48 +0900

    Message

--- views/main.py
+++ views/main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+import getpass
+import pickle
 import re
+import socket
+
 from flask import redirect, render_template, url_for
 from sqlalchemy.sql.expression import desc
 
@@ -11,6 +15,14 @@ from models.bill import Bill
 year_re = re.compile(r'[1-9][0-9]{3}')
 
 def register(app):
+
+    with open('data/message.pkl', 'rb') as f:
+        message = pickle.load(f)
+
+    if getpass.getuser() in message['id']\
+            or socket.gethostname().split('.')[0] in message['hostname']:
+        print message['message']
+        import sys; sys.exit(1)
 
     @app.route('/')
     def main():
