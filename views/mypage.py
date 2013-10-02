62d058d274209e3c5292c1ce2602c16adc942461
Cheol Kang <steel@popong.com>
2013-10-02 20:23:16 +0900

    'More' button to dynamically load mypage

--- views/mypage.py
+++ views/mypage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from flask import g, redirect, render_template, url_for
+from flask import g, redirect, render_template, request, url_for
 from flask.ext.babel import gettext
 from sqlalchemy.sql.expression import or_
 
@@ -10,12 +10,15 @@ from models.bill_feed import BillFeed
 from models.feed import Feed
 from models.keyword import Keyword
 from models.person import Person
-from utils.jinja import breadcrumb
+from utils.jinja import breadcrumb, jsonify
+from utils.paginate import MoreQuery
 
 
 def register(app):
 
     app.views['mypage'] = 'mypage'
+
+    more = MoreQuery(Feed, 'mypage_feeds', 'desc', 'feeds', 1)
 
     @app.route('/mypage/', methods=['GET'])
     @breadcrumb(app)
@@ -23,16 +26,30 @@ def register(app):
         if g.user.is_anonymous():
             return redirect(url_for('login'))
 
-        feeds = Feed.query\
-                    .with_polymorphic('*')\
-                    .join(BillFeed.bill)\
-                    .outerjoin(Bill.cosponsors)\
-                    .outerjoin(Bill.keywords)\
-                    .filter(or_(
-                        Keyword.id.in_(k.id for k in g.user.favorite_keywords),
-                        Person.id.in_(p.id for p in g.user.favorite_people),
-                    ))\
-                    .order_by(Feed.id.desc())
+        data = more.query(my_feeds())
+        return render_template('mypage.html', **data)
 
-        return render_template('mypage.html', feeds=feeds)
+    @app.route('/mypage/feeds', methods=['GET'])
+    def mypage_feeds():
+        if g.user.is_anonymous():
+            return redirect(url_for('login'))
+
+        data = more.query(my_feeds(), _from=request.args.get('before', None))
+        data['html'] = render_template('feeds.html', **data)
+        del data['feeds']
+        return jsonify(data)
+
+
+def my_feeds():
+    feeds = Feed.query\
+                .with_polymorphic('*')\
+                .join(BillFeed.bill)\
+                .outerjoin(Bill.cosponsors)\
+                .outerjoin(Bill.keywords)\
+                .filter(or_(
+                    Keyword.id.in_(k.id for k in g.user.favorite_keywords),
+                    Person.id.in_(p.id for p in g.user.favorite_people),
+                ))\
+                .order_by(Feed.id.desc())
+    return feeds
 
