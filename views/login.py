1ad9ea31e8829a253dd36c36e3c710f9458feda8
Cheol Kang <cornchz@gmail.com>
2013-11-20 23:45:38 +0900

    Expose login

--- views/login.py
+++ views/login.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from flask import g, render_template, redirect
+from flask import g, render_template, redirect, request, url_for
 from flask.ext.login import current_user, login_required, logout_user
 
 from social.apps.flask_app.template_filters import backends
@@ -9,19 +9,15 @@ from social.apps.flask_app.template_filters import backends
 
 def register(app):
 
-    @app.route('/login')
-    def login():
-        return render_template('login.html')
-
     @login_required
     @app.route('/done/')
     def done():
-        return render_template('login-done.html')
+        return redirect(request.referrer or url_for('main'))
 
     @app.route('/logout')
     def logout():
         logout_user()
-        return redirect('/')
+        return redirect(request.referrer or url_for('main'))
 
     @app.before_request
     def global_user():
