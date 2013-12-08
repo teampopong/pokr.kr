85dda5166893a28c17236771e20b7f1ebfe15933
Cheol Kang <cornchz@gmail.com>
2013-12-08 17:42:39 +0900

    Refactoring

--- utils/jinja.py
+++ utils/jinja.py
@@ -10,6 +10,7 @@ from werkzeug.urls import Href, url_unquote
 
 from controllers import controllers
 from models.assembly import term as assembly_term
+from models.election import current_age
 
 
 breadcrumb_home = ['home']
@@ -18,12 +19,21 @@ breadcrumb_home = ['home']
 def init_app(app):
     app.jinja_env.add_extension('jinja2.ext.loopcontrols')
     app.jinja_env.filters['jsonify'] = jsonify
-    app.jinja_env.globals.update(zip=zip, max=max, min=min, int=int, app=app)
-    app.jinja_env.globals.update(assembly_term=assembly_term)
-    app.jinja_env.globals.update(controllers=controllers)
-    app.jinja_env.globals.update(hasattr=hasattr)
-    app.jinja_env.globals.update(url_for_query=url_for_query)
-    app.jinja_env.globals.update(url_for_noencode=url_for_noencode)
+    app.jinja_env.globals.update(
+        hasattr=hasattr,
+        int=int,
+        max=max,
+        min=min,
+        zip=zip,
+    )
+    app.jinja_env.globals.update(
+        app=app,
+        assembly_term=assembly_term,
+        controllers=controllers,
+        current_age=current_age,
+        url_for_query=url_for_query,
+        url_for_noencode=url_for_noencode,
+    )
 
     app.breadcrumbed_views = {}
     @app.context_processor
