3494549d659a43d19e3a0076e2f51c58c6e0c581
Cheol Kang <cornchz@gmail.com>
2013-12-13 03:10:25 +0900

    More accurately guess the legislator of a region

--- controllers/region.py
+++ controllers/region.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from collections import defaultdict
+import json
+import os.path
 
-from flask import abort
+from flask import abort, current_app
 from sqlalchemy import func
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
@@ -18,21 +20,26 @@ class RegionController(Controller):
     model = 'region'
 
     @classmethod
-    def legislator_of(cls, region_id):
+    def legislator_of(cls, region_id, assembly_id=None):
         if not region_id:
             return None
+        if not assembly_id:
+            assembly_id = current_assembly_id()
 
-        assembly_id = current_assembly_id()
         region = Region.query.filter_by(id=region_id).one()
+        original_region = region
 
         legislator = None
         while not legislator and region:
+            legislators = region.candidates\
+                                .filter(Candidacy.assembly_id == assembly_id)\
+                                .filter_by(is_elected=True)
+
             try:
-                legislator = region.candidates\
-                                   .filter(Candidacy.assembly_id == assembly_id)\
-                                   .filter_by(is_elected=True)\
-                                   .one()
+                legislator = legislators.one()
             except MultipleResultsFound as e:
+                legislator = guess_legislator(legislators, original_region,
+                                              assembly_id)
                 break
             except NoResultFound as e:
                 region = region.parents.order_by(False)\
@@ -53,4 +60,16 @@ class RegionController(Controller):
         for person_id, assembly_id in officials_:
             res[assembly_id].append(person_id)
         return res
+
+
+def guess_legislator(legislators, region, assembly_id):
+    for legislator in legislators:
+        district = json.loads(legislator.extra_vars)\
+                       .get('assembly', {})\
+                       .get(str(assembly_id), {})\
+                       .get('district')
+        district_name = district.strip().split()[-1]
+        print legislator.name, district_name, region.district_name
+        if district_name == region.district_name:
+            return legislator
 
