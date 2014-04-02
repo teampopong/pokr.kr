7fe6e56d6a1cc3b8ad57f99f779d0670fc0a80c5
Cheol Kang <steel@popong.com>
2014-04-02 18:21:53 +0900

    'Debug' run option

--- run.py
+++ run.py
@@ -9,12 +9,15 @@ from pokr import app
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('port', type=int, nargs='?', default=50029)
+    parser.add_argument('-d', dest='debug', const=True, nargs='?')
     parser.add_argument('-l', dest='locale', default='auto',
                         help='force locale (e.g. en, kr)')
     return parser.parse_args()
 
 
 def apply_args(app, args):
+    if not args.debug is None:
+        app.debug = args.debug
     if args.locale in app.LOCALES:
         app.babel.force_locale(args.locale)
 
