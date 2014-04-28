5b783e7016ce3584e97f0f964414455ed9c1446b
Lucy Park <me@lucypark.kr>
2014-04-28 21:02:22 +0900

    Input detailed format of bill sponsors

--- pokr/scripts/bill.py
+++ pokr/scripts/bill.py
@@ -205,6 +205,9 @@ def extract_bill(record):
 
 
 def insert_cosponsorships(session, bill, cosponsors):
+    if isinstance(cosponsors[0], dict):
+        cosponsors = [c['name_kr'] for c in cosponsors]
+
     existing_cosponsor_ids = [c.id for c in bill.cosponsors]
     cosponsor_ids = []
     for proposer in cosponsors:
