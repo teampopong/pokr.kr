3e7100232fcf9545c736e85ab4da5345f0752885
Cheol Kang <cornchz@gmail.com>
2014-10-08 21:56:16 +0900

    In case that no party color exists

--- pokr/static/js/popong.js
+++ pokr/static/js/popong.js
@@ -101,4 +101,14 @@ $('.btn-show').live('click.show', function () {
     return false;
 });
 
+window.getRandomColor = function () {
+    // source: http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 }());
