95a8424798059b8d36e3de08e20282c2f8f819dd
Lucy Park <me@lucypark.kr>
2016-04-23 16:26:08 +0900

    Datatables server-side rendering

--- pokr/views/person.py
+++ pokr/views/person.py
@@ -6,7 +6,7 @@ import operator
 import time
 from collections import defaultdict
 
-from flask import abort, current_app, g, redirect, render_template, request, url_for
+from flask import abort, current_app, g, jsonify, redirect, render_template, request, url_for
 from flask.ext.babel import gettext
 from sqlalchemy import and_, func
 from sqlalchemy.orm import undefer_group
@@ -63,6 +63,58 @@ def register(app):
     @app.route('/person/all-names.json', methods=['GET'])
     def person_all_names():
         return person_names_json
+
+
+    @app.route('/person/list', methods=['GET'])
+    def people_list():
+
+        def wrap(data):
+            return [{
+                'DT_RowId': d.id,
+                'DT_RowClass': 'clickable',
+                'image': '<img src="%s" width="30px" height="40px">' % d.image,
+                'name': d.name,
+                'gender': '여' if d.gender=='f' else '남',
+                'birthday': d.birthday_year,
+                'cur_party': d.cur_party.name,
+                'nelected': d.nelected
+                } for d in data]
+
+        election_type = request.args.get('election_type', 'assembly')
+        assembly_id = int(\
+                request.args.get('assembly_id', current_parliament_id(election_type)) or 0)
+
+        draw = int(request.args.get('draw', 1))  # iteration number
+        start = int(request.args.get('start', 0))  # starting row's id
+        length = int(request.args.get('length', 10))  # number of rows in page
+
+        # order by
+        columns = ['image', 'name', 'gender', 'birthday', 'cur_party', 'nelected']
+        if draw==1:
+            order_column = func.random()  # shuffle rows
+        elif request.args.get('order[0][column]'):
+            order_column = columns[int(request.args.get('order[0][column]'))]
+            if request.args.get('order[0][dir]', 'asc')=='desc':
+                order_column += ' desc'
+        else:
+            order_column = None
+
+        officials = Person.query.join(Candidacy)\
+                                .filter(and_(Candidacy.type==election_type,
+                                             Candidacy.assembly_id==assembly_id,
+                                             Candidacy.is_elected==True))\
+                                .order_by(order_column)
+
+        filtered = officials.offset(start).limit(length)
+
+        response = {
+            'draw': draw,
+            'data': wrap(filtered),
+            'recordsTotal': officials.count(),
+            'recordsFiltered': officials.count()
+        }
+        return jsonify(**response)
+
 
     # 사람
     @app.route('/person/<int:id>', methods=['GET'])
