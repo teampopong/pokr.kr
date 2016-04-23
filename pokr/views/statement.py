19730dd82f19a03fd40e772a3b0ac2985434b0f8
Lucy Park <me@lucypark.kr>
2016-04-23 19:57:25 +0900

    Add before and after links for each statement

--- pokr/views/statement.py
+++ pokr/views/statement.py
@@ -7,6 +7,7 @@ import re
 
 from flask import abort, current_app, render_template, redirect, request, send_file, url_for
 from flask.ext.babel import gettext, format_date
+from sqlalchemy import and_
 from sqlalchemy.orm import undefer, undefer_group
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -38,4 +39,14 @@ def register(app):
         except NoResultFound, e:
             abort(404)
 
-        return render_template('statement.html', statement=statement, glossary_js=glossary_js)
+        before_statement = Statement.query.filter(and_(\
+                Statement.meeting_id==statement.meeting_id,
+                Statement.sequence==statement.sequence - 1)).first()
+
+        next_statement = Statement.query.filter(and_(\
+                Statement.meeting_id==statement.meeting_id,
+                Statement.sequence==statement.sequence + 1)).first()
+
+        return render_template('statement.html', statement=statement,\
+                before=before_statement, next=next_statement,
+                glossary_js=glossary_js)
